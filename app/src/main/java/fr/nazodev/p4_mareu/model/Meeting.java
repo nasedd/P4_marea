9e1d1387ddf080b90947582bae11e356a2e76316
Nadir Seddaoui <seddaoui.nadir@gmail.com>
2022-03-25 15:21:40 +0100

    datePicker & timePicker

--- app/src/main/java/fr/nazodev/p4_mareu/model/Meeting.java
+++ app/src/main/java/fr/nazodev/p4_mareu/model/Meeting.java
@@ -1,4 +1,8 @@
 package fr.nazodev.p4_mareu.model;
 
+import java.util.Date;
+
 public class Meeting {
+
+    Date date;
 }
--- app/src/main/java/fr/nazodev/p4_mareu/user_interface/MainActivity.java
+++ app/src/main/java/fr/nazodev/p4_mareu/user_interface/MainActivity.java
@@ -1,16 +1,28 @@
 package fr.nazodev.p4_mareu.user_interface;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 
 import fr.nazodev.p4_mareu.R;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = findViewById(R.id.viewpager);
         viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
 
+
     }
 
     @Override
@@ -27,4 +40,75 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.layout_menu, menu);
         return true;
     }
+
+
+    MaterialDatePicker materialDatePicker = MaterialDatePicker.Builder.datePicker()
+                                                            .setTitleText("Select dates")
+                                                            .build();
+    MaterialTimePicker materialTimePicker =  new MaterialTimePicker.Builder()
+                                                                        .setTimeFormat(TimeFormat.CLOCK_24H)
+                                                                        .setHour(12)
+                                                                        .setMinute(10)
+                                                                        .build();
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.date_filter){
+            //buttonSelectDate();
+            materialDatePicker.show(getSupportFragmentManager(),"tag");
+            materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                @Override public void onPositiveButtonClick(Object selection) {
+
+                    Toast.makeText(MainActivity.this, materialDatePicker.getHeaderText() + "-" ,Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+
+
+
+        }
+
+        if(item.getItemId() == R.id.room_1){
+            materialTimePicker.show(getSupportFragmentManager(),"tag");
+            materialTimePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, materialTimePicker.getHour() + " : " + materialTimePicker.getMinute() ,Toast.LENGTH_SHORT).show();
+                }
+
+            });
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+
+    private void buttonSelectDate() {
+
+        // Date Select Listener.
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year,
+                                  int monthOfYear, int dayOfMonth) {
+
+                //editTextDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                Toast.makeText(MainActivity.this, dayOfMonth + "-" + (monthOfYear + 1) + "-" + year,Toast.LENGTH_SHORT).show();
+            }
+        };
+
+
+        // Show
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, dateSetListener, 2022,2,4);
+        datePickerDialog.show();
+
+    }
 }
+
+//Toast.makeText(MainActivity.this, "dayOfMonth + "-" + (monthOfYear + 1) + "-" + year",Toast.LENGTH_SHORT).show();
