047d3e1e5a050ef2f3d0ff425c68f30703c1ed73
Nadir Seddaoui <seddaoui.nadir@gmail.com>
2022-03-26 20:38:11 +0100

    add functionality for subject, date & time editText

--- app/src/main/java/fr/nazodev/p4_mareu/user_interface/NewMeetingActivity.java
+++ app/src/main/java/fr/nazodev/p4_mareu/user_interface/NewMeetingActivity.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import com.google.android.material.timepicker.TimeFormat;
 
@@ -36,9 +38,12 @@ public class NewMeetingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_meeting);
 
+        //************* RecyclerView adapter ***********************//
         RecyclerView recyclerView = findViewById(R.id.participants_recycler_view);
         recyclerView.setAdapter(new ParticipantRecyclerViewAdapter(emailList));
 
+        //************* Modify ENTER_KEY behavior *****************//
+        //************ add email or show error ********************//
         EditText emailInput = findViewById(R.id.edittext_participants);
         emailInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -53,32 +58,67 @@ public class NewMeetingActivity extends AppCompatActivity {
                     }else {
                         emailInput.setError("email not valid !");
                     }
-
                     handled = true;
                 }
                 return handled;
             }
         });
 
+        //******************* Array and Adapter for the ROOM dropdown menu **********************//
         AutoCompleteTextView dropdownLocation = findViewById(R.id.autoCompleteTextView);
         String[] dropdownList = getResources().getStringArray(R.array.room_list);
         ArrayAdapter arrayAdapter = new ArrayAdapter(this,R.layout.dropdown_room_item,dropdownList );
         dropdownLocation.setAdapter(arrayAdapter);
 
+        //****************** set Date *******************************//
+        EditText editTextDate = findViewById(R.id.editTextDate);
+        setDate(editTextDate);
 
-        MaterialDatePicker materialDatePicker = MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Select dates")
-                .build();
+        //****************** set Time *******************************//
+        EditText editTextTime = findViewById(R.id.editTextTime);
+        setTime(editTextTime);
+
+
+
+
+    }
+
+    private void setTime(EditText editText) {
         MaterialTimePicker materialTimePicker =  new MaterialTimePicker.Builder()
                 .setTimeFormat(TimeFormat.CLOCK_24H)
                 .setHour(12)
                 .setMinute(10)
                 .build();
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    materialTimePicker.show(getSupportFragmentManager(),"tag");
+                    materialTimePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String timeSelected = materialTimePicker.getHour() + ":" + materialTimePicker.getMinute();
+                            editText.setText(timeSelected);
+                        }
 
+                    });
+                }
+            }
+        });
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialTimePicker.show(getSupportFragmentManager(),"tag");
+                materialTimePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String timeSelected = materialTimePicker.getHour() + ":" + materialTimePicker.getMinute();
+                        editText.setText(timeSelected);
+                    }
 
-
-
-
+                });
+            }
+        });
     }
 
     private boolean isEmailValidate(Editable email) {
@@ -88,6 +128,39 @@ public class NewMeetingActivity extends AppCompatActivity {
         }else{
             return false;
         }
+    }
+
+    private void setDate(EditText editText){
+        MaterialDatePicker materialDatePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select dates for the meeting")
+                .build();
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    //Toast.makeText(NewMeetingActivity.this,"heaaay",Toast.LENGTH_SHORT).show();
+                    materialDatePicker.show(getSupportFragmentManager(),"tag");
+                    materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                        @Override public void onPositiveButtonClick(Object selection) {
+                            editText.setText(materialDatePicker.getHeaderText());
+
+                        }
+                    });
+                }
+
+            }
+        });
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getSupportFragmentManager(),"tag");
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                    @Override public void onPositiveButtonClick(Object selection) {
+                        editText.setText(materialDatePicker.getHeaderText());
+                    }
+                });
+            }
+        });
     }
 
 
